f1a7177be029a4af909ab6b638be9dd325910777
Alexandre Laureano Prata Gillet <57054750+alexandrelpgillet@users.noreply.github.com>
2024-09-22 20:57:40 -0300

    added time execution in main.c

--- main.c
+++ main.c
@@ -6,20 +6,40 @@
 #include "heap_sort.h"
 #include "swap.h"
 #include <stdio.h>
+#include <time.h>
+
 
 int main() {
 
-    int V[10] = { 10,2,33,4,1000,80,713,81442,93,1130 };
+    clock_t start , end;
+    double cpu_time_used;
 
-    //selection_sort(V, 10);
-    //insertion_sort(V, 10);
-    //bubble_sort(V, 10);
-    //merge_sort(V, 0, 9);
-    //heap_sort(V ,10);
 
-    
+    int V[500000];
 
-    for (int i = 0; i < 10; i++)
+    for(int i = 0 ; i<500000; i++) {
+
+        scanf("%d", &V[i]);
+    }
+
+
+
+    start = clock();
+
+    //selection_sort(V, 500000);
+    //insertion_sort(V, 500000);
+    //bubble_sort(V, 500000);
+    //merge_sort(V, 0, 499999);
+    heap_sort(V ,500000);
+
+    end = clock();
+
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+
+    printf("Tempo de execução: %f segundos\n", cpu_time_used);
+
+
+    for (int i = 0; i < 100000; i++)
     {
         printf("%d|", V[i]);
     }
